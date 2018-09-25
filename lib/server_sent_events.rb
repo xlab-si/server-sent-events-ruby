2d43e73ccda90dbbbf1b72e0de0049adb71ba4b6
Tadej Borovšak <tadej.borovsak@xlab.si>
2018-09-25 10:20:30 +0200

    Update documentation for listen helper
    Developer documentation contained some really bad mistakes and was
    missing a parameter description. This commit rectifies those two
    issues.

--- lib/server_sent_events.rb
+++ lib/server_sent_events.rb
@@ -7,7 +7,7 @@ require "server_sent_events/client"
 require "server_sent_events/parser"
 
 module ServerSentEvents
-  # Convenience method go the up-and running fast.
+  # Convenience method to get up-and-running fast.
   #
   # In order to start listening to server events, this is all the code that we
   # need:
@@ -15,6 +15,10 @@ module ServerSentEvents
   #     ServerSentEvents.listen("http://example.com") do |event|
   #       puts event
   #     end
+  #
+  # @param address [String, URI] SSE endpoint
+  # @param headers [Hash] HTTP headers to use when connecting
+  # @param callback code block that should be executed on event arrival
   def self.listen(address, headers = {}, &callback)
     Client.new(URI(address), Parser.new, headers).listen(&callback)
   end
